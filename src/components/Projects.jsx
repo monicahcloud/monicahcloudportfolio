1d3707cf41ce7cc3a8b989df94dba4e0282015d7
Monicah Cloud <monicahcloud@gmail.com>
2024-10-27 20:41:38 -0500

    nav tech, service and about are responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,45 @@
 import ProjectsCard from './ProjectsCard'
-import { projects } from '../data'
+
 
 const Projects = () => {
-
+const projects = [
+  {
+    id: 1,
+    url: 'https://www.crownoflocs.com',
+    title: 'Crown of Locs and Natural Hair LLC.',
+    topic: 'small business',
+    image: '../src/images/2-removebg-preview.png',
+    info: ' This is a single page website with routing/navigation',
+    git: 'https://github.com/monicahcloud/crownoflocs',
+  },
+  {
+    id: 2,
+    url: 'https://www.nerahspassion.com',
+    title: 'Nerahs Passion',
+    topic: 'single page business website',
+    image: '../src/images/3-removebg-preview.png',
+    info: 'This is a single page website. ',
+    git: 'https://github.com/monicahcloud/nerahspassion',
+  },
+  {
+    id: 3,
+    url: 'https://www.google.com',
+    title: 'EPIC',
+    topic: 'non-profit website',
+    image: '../src/images/4-removebg-preview.png',
+    info: 'This project was built using ReactJS. ',
+    git: 'http://github.com/monicahcloud',
+  },
+  {
+    id: 4,
+    url: 'https://www.brp.com',
+    title: " Monicah's Journey",
+    topic: 'personal blog',
+    image: '../src/images/9-removebg-preview.png',
+    info: 'This project was built using ReactJS. ',
+    git: 'http://github.com/monicahcloud',
+  },
+]
   return (
     <>
       <section className="portfolioHeading" id="projects">
