962410dfda1a9d847bc90b9f9a53528dcf65c1c8
MONICAH C CLOUD <monicahcloud@gmail.com>
2025-03-08 14:26:12 -0500

    uncommented home

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -1,15 +1,15 @@
 import logo from "../../assets/images/mono5.png";
-
+import { Link } from "react-scroll";
 function Logo() {
   return (
     <>
-      <a href="home" className="">
+      <Link spy={true} smooth={true} duration={500} offset={-120} to="contact">
         <img
           src={logo}
           alt="logo"
           className="w-40 block mt-[-4rem] mb-[-4rem] ml-02 "
         />
-      </a>
+      </Link>
     </>
   );
 }
