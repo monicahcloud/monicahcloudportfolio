f635a8e3a77598f4ab4cd6d1f1d7bc0e75e0aa85
MONICAH C CLOUD <monicahcloud@gmail.com>
2025-03-08 14:22:14 -0500

    uncommented home

--- src/data.jsx
+++ src/data.jsx
@@ -8,11 +8,11 @@ import { nanoid } from "nanoid";
 import { FaHtml5, FaJs, FaReact } from "react-icons/fa";
 
 export const links = [
-  // {
-  //   id: nanoid(),
-  //   href: "#home",
-  //   text: "home",
-  // },
+  {
+    id: nanoid(),
+    href: "#home",
+    text: "home",
+  },
 
   {
     id: nanoid(),
