7ed49c620ac8b4dd53a5e8365f45fa6556a4bc51
MONICAH C CLOUD <monicahcloud@gmail.com>
2025-03-09 09:50:35 -0400

    redo hero

--- src/components/navbar/NavBarMain.jsx
+++ src/components/navbar/NavBarMain.jsx
@@ -3,20 +3,24 @@ import { links } from "../../data";
 
 import Logo from "./Logo";
 import NavLink from "./NavLink";
+import NavbarBtn from "./NavbarBtn";
 
 const NavBarMain = () => {
   return (
     <>
-      <nav className="sticky top-0 z-[20] bg-slate-900 flex border-orange-400 justify-between items-center px-5 py-4">
-        <div className="flex items-center ml-10 justify-between w-full">
-          {/* Logo */}
+      <nav className="border-b border-b-orange-600">
+        <div className="mx-10 py-4 px-8 flex justify-between items-center sticky top-0 z-[20] ">
           <div className="flex-shrink-0">
-            <Logo />
+            <h1 className=" text-2xl font-bold tracking-wider text-cyan-600">
+              Monicah<span className="text-white">Cloud</span>{" "}
+            </h1>
           </div>
-
-          {/* Links Container (centered on larger screens) */}
-
-          <NavLink />
+          <div>
+            <NavLink />
+          </div>
+          <div>
+            <NavbarBtn />
+          </div>{" "}
         </div>
       </nav>
     </>
