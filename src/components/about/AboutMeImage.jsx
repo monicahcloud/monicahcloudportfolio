9d43a1ad78197c4f997fc758f9af9d7217df4b02
MONICAH C CLOUD <monicahcloud@gmail.com>
2025-03-09 21:20:09 -0400

    complete

--- src/components/about/AboutMeImage.jsx
+++ src/components/about/AboutMeImage.jsx
@@ -2,15 +2,15 @@ import aboutImg from "../../assets/images/7-removebg-preview.png";
 
 const AboutMeImage = () => {
   return (
-    <div className=" mx-10 h-[500px] w-full relative">
-      <div className="h-[500px] w-full rounded-[100px] absolute overflow-hidden">
+    <div className=" mx-10 h-[400px] md:h-[500px] w-full relative">
+      <div className="h-[400px] md:h-[500px] w-full rounded-[100px] absolute overflow-hidden">
         <img
           src={aboutImg}
           alt="About Me Image"
           className="h-full w-auto object-cover scale-x-[-1]"
         />
       </div>
-      <div className="h-[500px] w-[400px] bg-orange-300 absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10"></div>
+      <div className="h-[400px] w-[300px] md:h-[500px] md:w-[400px] bg-orange-300 absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10"></div>
     </div>
   );
 };
