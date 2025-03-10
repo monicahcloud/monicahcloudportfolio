16db2f1ba87a56dfb45fea1f58e58486e1c6c94e
MONICAH C CLOUD <monicahcloud@gmail.com>
2025-03-09 21:24:58 -0400

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Footer from "./components/footer/Footer";
 function App() {
   return (
     <>
-      <main className="">
+      <main className="container mx-auto max-w-[1200px]">
         <Navbar />
         <Hero />
         <SubHero />
