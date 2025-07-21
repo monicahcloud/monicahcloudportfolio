501a3701d237fe75aaa0803eef47d9a740b316d5
monicahcloud <monicahcloud@gmail.com>
2025-07-21 13:38:26 -0500

    update contact info

--- src/components/about/AboutmeText.jsx
+++ src/components/about/AboutmeText.jsx
@@ -4,9 +4,13 @@ const AboutMeText = () => {
   return (
     <div className="flex flex-col items-center text-center mx-5">
       <p className="text-white text-2xl pt-10">
-        I help businesses grow. By understanding your needs, I turn your
-        challenges into simple, effective solutions by establishing custom, easy
-        to use systems that make a real difference for your business.
+        I’m a creative problem solver who helps people bring their ideas to life
+        with clarity and purpose. Whether it’s building a brand, designing a
+        website, or turning messy data into simple insights, I blend strategy,
+        storytelling, and design to create experiences that truly connect. I
+        care deeply about making things that feel thoughtful, human, and
+        impactful—because I believe every voice deserves to be seen, heard, and
+        understood.
       </p>
       <button className="border border-cyan-300 rounded-full py-2 px-8 w-1/2 md:w-1/3 justify-center text-lg flex gap-2 items-center mt-8  hover:bg-orange-300 transition-all duration-500 cursor-pointer ">
         <a
